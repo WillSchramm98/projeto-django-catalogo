a4d04be1948edb81ff7f952c799959d85ac7e666
Aluno G <gabri888999@gmail.com>
2026-02-10 16:29:33 -0300

    Correção no settings e criaçao do build.sh

--- projeto-django-catalogo-main/build.sh
+++ projeto-django-catalogo-main/build.sh
@@ -1,4 +1,25 @@
 #!/usr/bin/env bash
+# Interrompe a execução se algum comando falhar
+set -o errexit
+
+# Instala as dependências
 pip install -r requirements.txt
+
+# Coleta arquivos estáticos
 python manage.py collectstatic --noinput
+
+# Aplica migrações do banco de dados
 python manage.py migrate
+
+# Cria o superusuário se ele não existir
+if [ "$SUPERUSER_NAME" ]; then
+  python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='$SUPERUSER_NAME').exists():
+    User.objects.create_superuser('$SUPERUSER_NAME', '$SUPERUSER_EMAIL', '$SUPERUSER_PASSWORD')
+    print("Superuser criado com sucesso!")
+else:
+    print("Superuser já existe.")
+END
+fi
